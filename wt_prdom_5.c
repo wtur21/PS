becd2ec3830466c7f4b620fa814722ed8756d365
wtur21 <77536218+wtur21@users.noreply.github.com>
2021-04-13 16:22:14 +0200

    Add files via upload

---
+++ wt_prdom_5.c
@@ -0,0 +1,32 @@
+// Programowanie strukturalne
+
+// Praca domowa 5
+
+// (cw. 10-04-21r.)
+
+//******************************************************************************************************
+
+// Zadanie 3.2.8
+
+// https://github.com/wtur21/PS/blob/main/wt_3_2_8.cpp
+
+//******************************************************************************************************
+
+// Zadanie 3.2.12
+
+int * rez(unsigned int n)
+{
+return malloc(n * sizeof(double));
+}
+
+//******************************************************************************************************
+
+// Zadanie 3.2.14
+
+int spr(int (* funkcja1)(int a), int (* funkcja2)(int b), unsigned int n)
+{
+if()
+    return true;
+else
+    return false;
+}
